801a963193f787b838a03e44ecfac7520463b215
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-09-27 09:05:11 -0500

    feat: Create CustomUser

--- src/account/models.py
+++ src/account/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+  email = models.EmailField(_('email address'), unique=True)
+
+  objects = CustomUserManager
+
+  def __str__(self):
+    return self.email
--- src/config/settings.py
+++ src/config/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# User setting
+AUTH_USER_MODEL = 'account.CustomUser'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
